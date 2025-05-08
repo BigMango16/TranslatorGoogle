84de03a82bf854f4a2fabcf54305da4d55bd20c6
Valeri Kamburov <v074807@gmail.com>
2025-05-08 11:40:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,24 +30,3 @@ if text:
     translator = Translator()
     translation = translator.translate(text, dest=language_code)
     st.write(f'Преведеният текст на {language_name}: {translation.text}')
-
-
-
-
-from gtts import gTTS
-import os
-import streamlit as st
-
-# Функция за озвучаване
-def text_to_speech(text, lang='en'):
-    tts = gTTS(text=text, lang=lang)
-    tts.save("translation.mp3")
-    return "translation.mp3"
-
-# Примерна интеграция в Streamlit
-translated_text = "Тук е вашият превод."  # Заменете с реалния преведен текст
-st.write(translated_text)
-
-if st.button("Озвучаване на превода"):
-    audio_file = text_to_speech(translated_text, lang='bg')  # Пример за български
-    st.audio(audio_file, format="audio/mp3")
