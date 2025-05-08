bd6e00899e94f5f18e9cdf9f1a53463b377d8f17
Valeri Kamburov <v074807@gmail.com>
2025-05-08 11:34:59 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,26 @@ st.title('Приложение за превод на текст')
 # Входен текст
 text = st.text_input("Въведете текст за превод:")
 
+# Списък с налични езици
+languages = {
+    "Английски": "en",
+    "Френски": "fr",
+    "Немски": "de",
+    "Италиански": "it",
+    "Испански": "es",
+    "Руски": "ru",
+    "Китайски (опростен)": "zh-cn",
+    "Японски": "ja",
+    "Корейски": "ko",
+    "Арабски": "ar"
+}
+
 # Избор на език
-language = st.selectbox("Изберете език за превод", ["en", "fr", "de", "it", "es"])
+language_name = st.selectbox("Изберете език за превод", list(languages.keys()))
+language_code = languages[language_name]
 
 # Превод
 if text:
     translator = Translator()
-    translation = translator.translate(text, dest=language)
-    st.write(f'Преведеният текст на {language}: {translation.text}')
+    translation = translator.translate(text, dest=language_code)
+    st.write(f'Преведеният текст на {language_name}: {translation.text}')
